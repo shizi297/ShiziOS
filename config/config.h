ddbeed78018554bdfcb8bb3001c0c8a8fe875c36
shizi297 <shizi@shizi>
2026-05-19 03:22:10 +0800

    Add cross-compile support

--- config/config.h
+++ config/config.h
@@ -3,8 +3,7 @@
  * SPDX-FileCopyrightText: 2025 shizi <https://github.com/shizi297>
  */
  
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #define ARCH_X86_64   0
 #define ARCH_ARM      1
@@ -18,5 +17,3 @@
 #define TASK_ID_MAX ((1ULL << 22) - 1)  // 任务管理ID的最大数量
 
 #define ARCH          ARCH_X86_64
-
-#endif // CONFIG_H 
