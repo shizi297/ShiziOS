cbd86055a866a24b1f9cc8dacee3f2552eb232fb
shizi297 <shizi@shizi>
2026-05-27 22:03:54 +0800

    Refactor initcall sections with array syntax and retain attribute

--- kernel/include/initcall.h
+++ kernel/include/initcall.h
@@ -9,16 +9,16 @@
 #define INITCALL(group, level, fn) \
     static void (*__initcall_##fn)(void) \
     __attribute__((section(".initcall_" #group "_" #level))) \
-    __attribute__((used)) = fn
+    __attribute__((used, retain)) = fn
 
 // 按优先级顺序调用该段内所有注册的函数
 #define initcall(group, level) \
     do { \
-        extern void (*__start_initcall_##group##_##level)(void); \
-        extern void (*__stop_initcall_##group##_##level)(void); \
+        extern void (*__start_initcall_##group##_##level[])(void); \
+        extern void (*__stop_initcall_##group##_##level[])(void); \
         void (**fn)(void); \
-        for (fn = &__start_initcall_##group##_##level; \
-             fn < &__stop_initcall_##group##_##level; fn++) { \
+        for (fn = __start_initcall_##group##_##level; \
+             fn < __stop_initcall_##group##_##level; fn++) { \
             (*fn)(); \
         } \
     } while(0)
